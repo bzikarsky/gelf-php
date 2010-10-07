2c50b883ba4b6c8cc960171d0a97f76a8c6765d8
Lennart Koopmann <lennart@scopeport.org>
2010-10-07 19:58:56 +0200

    no more debug data

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
 require('gelf.php');
-require('longmessage');
 
 $gelf = new GELFMessage('localhost', 12201);
 
 $gelf->setShortMessage('something is broken.');
-//$gelf->setFullMessage($longData);
 $gelf->setFullMessage("lol full message!");
 $gelf->setHost('somehost');
 $gelf->setLevel(2);
