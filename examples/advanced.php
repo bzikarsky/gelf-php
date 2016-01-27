e211dda75906e72d4b943bcaa700688116bd9a46
Matt Thomas <matt@betweenbrain.com>
2016-01-27 10:29:46 -0500

    Update comments in exmaples/advanced.php
    Fix a typo in the UDP transport comment and make it more succinct.

--- examples/advanced.php
+++ examples/advanced.php
@@ -14,9 +14,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // We need a transport - UDP via port 12201 is standard.
 $transport = new Gelf\Transport\UdpTransport("127.0.0.1", 12201, Gelf\Transport\UdpTransport::CHUNK_SIZE_LAN);
 
-// While the UDP transport is itself a publisher, we wrap it in a real Publisher for convenience
-// A publisher allows for message validation before transmission, and it calso supports to send messages
-// to multiple backends at once
+// While the UDP transport is itself a publisher, we wrap it in a real Publisher for convenience.
+// A publisher allows for message validation before transmission, and also supports sending 
+// messages to multiple backends at once.
 $publisher = new Gelf\Publisher();
 $publisher->addTransport($transport);
 
