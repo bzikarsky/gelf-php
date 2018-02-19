dbb272b1545d6bd61dce11ec30dc64ac8348d872
Олег Максименко <oleg.39style@gmail.com>
2018-02-19 09:17:17 +0200

    Replace strlen with binary safe variant
    - Added package paragonie/constant_time_encoding into composer.json
    - Update \Gelf\Transport\StreamSocketClient class: replaced "strlen" on ParagonIE\ConstantTime\Binary::safeStrlen
    - Removed the check for mbstring.func_overload

--- src/Gelf/Transport/StreamSocketClient.php
+++ src/Gelf/Transport/StreamSocketClient.php
@@ -12,6 +12,7 @@
 namespace Gelf\Transport;
 
 use RuntimeException;
+use ParagonIE\ConstantTime\Binary;
 
 /**
  * StreamSocketClient is a very simple OO-Wrapper around the PHP
@@ -199,7 +200,7 @@ class StreamSocketClient
     public function write($buffer)
     {
         $buffer = (string) $buffer;
-        $bufLen = strlen($buffer);
+        $bufLen = Binary::safeStrlen($buffer);
 
         $socket = $this->getSocket();
         $written = 0;
@@ -214,7 +215,7 @@ class StreamSocketClient
                 $failed = true;
                 $errorMessage .= ": $errstr ($errno)";
             });
-            $byteCount = fwrite($socket, substr($buffer, $written));
+            $byteCount = fwrite($socket, Binary::safeSubstr($buffer, $written));
             restore_error_handler();
 
             if ($byteCount === 0 && defined('HHVM_VERSION')) {
